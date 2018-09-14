a1b36e7c3f6023537c3848a729f51fbdc926716e
JacobLau0513 <yiliu0513@gmail.com>
2018-09-13 23:05:49 -0500

    Add files via upload

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,14 +13,19 @@ def max_2_sum arr
   # YOUR CODE HERE
   return 0 if arr.empty?
   return arr[0] if arr.length == 1
-  return arr.max(2).reduce(:+)
+  return arr.sort[-2..-1].sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
-  return false if arr.length == 1
-  arr.each_with_index do |val, id|
-    return true if arr.[id+1..-1].include? n-val
+  return false if arr.empty? or arr.length == 1
+  s = Set.new
+  arr.each do |val|
+    if s.include?(n-val)
+      return true
+    else
+      s.add(val)
+    end
   end
   return false
 end
@@ -39,7 +44,7 @@ end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  return false if /[a-zA-Z^$2-9*]/.match(s)
+  return true if s == "0"
   return true if /^[10]*00$/.match(s) && s.to_i % 2 == 0
 end
 
